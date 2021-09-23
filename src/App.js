9f7d809bbe1b01fa0c02d458a01f0769ddfc1c5c
oluseyi-ged <oluseyi.ged@gmail.com>
2021-09-23 17:58:16 +0100

    adding to commerce cart

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Products, Navbar } from "./components"
 
 const App = () => {
   const [products, setProducts] = useState([])
+  const [cart, setCart] = useState({})
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list()
@@ -13,16 +14,27 @@ const App = () => {
     setProducts(data)
   }
 
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve())
+  }
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity)
+
+    setCart(item.cart)
+  }
+
   useEffect(() => {
     fetchProducts()
+    fetchCart()
   }, [])
 
-  console.log(products)
+  console.log(cart)
 
   return (
     <div>
       <Navbar />
-      <Products products={products} />
+      <Products products={products} onAddToCart={handleAddToCart} />
     </div>
   )
 }
