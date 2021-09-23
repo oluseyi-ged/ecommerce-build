e169e8c5c0bd3c5f05bd77e4f3d6f426f5f25285
oluseyi-ged <oluseyi.ged@gmail.com>
2021-09-23 12:42:43 +0100

    product grid set up in component

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,38 @@
+import React from "react"
+import { Grid } from "@material-ui/core"
+import Product from "./Product/Product"
+
+const products = [
+  {
+    id: 1,
+    name: "Shoes",
+    description: "Reebok",
+    price: "$5",
+    image:
+      "https://footwearnews.com/wp-content/uploads/2021/04/rebook-club-c-85-1.jpg",
+  },
+  {
+    id: 2,
+    name: "Laptop",
+    description: "Apple Macbook",
+    image:
+      "https://cdn.mos.cms.futurecdn.net/TAVcYGYSCVprN9LRUzQqbD-970-80.jpg.webp",
+    price: "$25",
+  },
+]
+
+const Products = () => {
+  return (
+    <main>
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  )
+}
+
+export default Products
