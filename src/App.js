d08e784f72b1cceee8bbfb5dc853e496bcf97b7a
oluseyi-ged <oluseyi.ged@gmail.com>
2021-09-23 13:08:24 +0100

    navbar and card layout fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react"
 import "./App.css"
-import Products from "./components/Products/Products"
+// import Navbar from "./components/Navbar/Navbar"
+// import Products from "./components/Products/Products"
+
+import { Products, Navbar } from "./components"
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Products />
     </div>
   )
